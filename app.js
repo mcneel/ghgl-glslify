ad6de6b3031c6a74623c3b96a49ebcfcd217337c
Steve Baer <steve@mcneel.com>
2019-04-22 21:21:54 -0700

    add post support for processing

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const glsl = require('glslify');
+const bodyParser = require('body-parser');
+
 const app = express()
+//app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
-var glsl = require('glslify');
 
 app.get('/', (req, res) => {
   var rc = glsl(`
@@ -17,4 +21,11 @@ app.get('/', (req, res) => {
   res.send(rc)
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.post('/process', function(req, res){
+  var code = req.body.code;
+  var processed = glsl(code);
+  res.send(processed);
+});
+
+
+app.listen(port, () => console.log(`ghgl-glslify app listening on port ${port}!`))
