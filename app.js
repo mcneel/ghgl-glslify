e76f0ecfd8e2f80c287db067882b67deca21975e
Steve Baer <steve@mcneel.com>
2019-04-22 21:25:46 -0700

    simplify app.js

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
-const express = require('express')
+const express = require('express');
 const glsl = require('glslify');
 const bodyParser = require('body-parser');
 
-const app = express()
-//app.use(bodyParser.urlencoded({extended: false}));
+const app = express();
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  var rc = glsl(`
-    #pragma glslify: noise = require('glsl-noise/simplex/3d')
-
-    precision mediump float;
-    varying vec3 vpos;
-    void main () {
-      gl_FragColor = vec4(noise(vpos*25.0),1);
-    }
-  `);
-  res.send(rc)
+  res.send('ghgl-glslify online');
 });
 
 app.post('/process', function(req, res){
