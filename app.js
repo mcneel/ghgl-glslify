8f43590f0945e18d7b1728a7f3a157695ca25bd0
Steve Baer <steve@mcneel.com>
2019-04-23 20:35:40 -0700

    add packages and package endpoints

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const glsl = require('glslify');
 const bodyParser = require('body-parser');
+const packageInfo = require('package-info');
+const fs = require('fs');
+
+var content = fs.readFileSync('package.json');
+var glslPackages = JSON.parse(content).dependencies;
+delete glslPackages['express'];
+delete glslPackages['glslify'];
+delete glslPackages['body-parser'];
+delete glslPackages['package-info'];
+
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,6 +19,16 @@ var port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.send('ghgl-glslify online');
+});
+
+app.get('/packages', async (req, res) => {
+  res.send(Object.keys(glslPackages));
+});
+
+app.get('/package/:packageName', async (req, res) => {
+  package = req.params.packageName;
+  let info = await packageInfo(package);
+  res.send(info);
 });
 
 app.post('/process', function(req, res){
