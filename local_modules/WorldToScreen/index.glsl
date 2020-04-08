977549c4f36e4cea75d9f6c1c94818b23631c14e
Steve Baer <steve@mcneel.com>
2020-04-07 17:08:00 -0700

    Fix syntax error

--- local_modules/WorldToScreen/index.glsl
+++ local_modules/WorldToScreen/index.glsl
@@ -1,4 +1,4 @@
-vec2 WorldToScreen(in vec3 point, in bool upperLeftOrigin, int mat4 w2s, in vec2 viewportSize)
+vec2 WorldToScreen(in vec3 point, in bool upperLeftOrigin, in mat4 w2s, in vec2 viewportSize)
 {
   vec4 screen = w2s * vec4(point, 1.0);
   screen.xy /= screen.w;
