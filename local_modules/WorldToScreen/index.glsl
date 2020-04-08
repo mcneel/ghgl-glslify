af656f84a66a0d8ef08187ea0bd608afd88c088e
Steve Baer <steve@mcneel.com>
2020-04-07 17:34:02 -0700

    Fix WorldToScreen typo

--- local_modules/WorldToScreen/index.glsl
+++ local_modules/WorldToScreen/index.glsl
@@ -1,12 +1,11 @@
 // converte a 3d world coordinate to a screen location
-vec2 WorldToScreen(in vec3 point, in bool upperLeftOrigin, in mat4 w2s, in vec2 viewportSize)
-{
+vec2 WorldToScreen(in vec3 point, in bool upperLeftOrigin, in mat4 w2s, in vec2 viewportSize) {
   vec4 screen = w2s * vec4(point, 1.0);
   screen.xy /= screen.w;
   if (!upperLeftOrigin) {
     screen.y = viewportSize.y - screen.y;
   }
-  return vec3(screen.x, screen.y);
+  return vec2(screen.x, screen.y);
 }
 
 
