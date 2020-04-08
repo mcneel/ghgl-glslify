4ee4a5bdd44ca120ddc130ee76720e840bcbab51
Steve Baer <steve@mcneel.com>
2020-04-07 17:00:23 -0700

    Add WorldToScreen function

---
+++ local_modules/WorldToScreen/index.glsl
@@ -0,0 +1,12 @@
+vec2 WorldToScreen(in vec3 point, in bool upperLeftOrigin, int mat4 w2s, in vec2 viewportSize)
+{
+  vec4 screen = w2s * vec4(point, 1.0);
+  screen.xy /= screen.w;
+  if (!upperLeftOrigin) {
+    screen.y = viewportSize.y - screen.y;
+  }
+  return vec3(screen.x, screen.y);
+}
+
+
+#pragma glslify: export(WorldToSreen)
